71bbe2c5928f266a4025b9ddf4c920c62430f355
Vitaly Magerya <magv@tx97.net>
2023-02-02 16:04:38 +0100

    Remove the "-1" from the CutAmplitudeGlue of the ghost
    The users are now expected to subtract the ghost diagrams
    manually.

--- amodel-qcd.m
+++ amodel-qcd.m
@@ -99,20 +99,24 @@ Amplitude[V[_, "HtT", fi1_, p1_, fi2_, p2_, fi3_, p3_]] :=
 CutAmplitudeGlue[F[f:"H", fi_, _, mom_], F[f_, fi_, _, mom_]] := 1
 CutAmplitudeGlue[F[f:"g", fi_, _, mom_], F[f_, fi_, _, mom_]] := (* -g_mn d_ab *)
   -delta[lor[fi], lor[fi] // AmpConjugate] delta[adj[fi], adj[fi] // AmpConjugate]
-(* The -1 here makes it so that the external ghost pair contributions
- * are subtracted. *)
-CutAmplitudeGlue[F[f:"c", fi_, _, mom_], F[f_, fi_, _, mom_]] := (* -1 * d_ab *)
-  -delta[adj[fi], adj[fi] // AmpConjugate]
-CutAmplitudeGlue[F[f:"C", fi_, _, mom_], F[f_, fi_, _, mom_]] := (* d_ab *)
+CutAmplitudeGlue[F[f:"c"|"C", fi_, _, mom_], F[f_, fi_, _, mom_]] := (* d_ab *)
   delta[adj[fi], adj[fi] // AmpConjugate]
 CutAmplitudeGlue[F[f:"Q", fi_, _, mom_], F[f_, fi_, _, mom_]] := (* p^slash d_ij d_f1f2 *)
-  gammachain[slash[mom], spn[fi], spn[fi] // AmpConjugate] delta[fun[fi], fun[fi] // AmpConjugate] deltaf[flv[fi], flv[fi] // AmpConjugate]
+  gammachain[slash[mom], spn[fi], spn[fi] // AmpConjugate] *
+  delta[fun[fi], fun[fi] // AmpConjugate] *
+  deltaf[flv[fi], flv[fi] // AmpConjugate]
 CutAmplitudeGlue[F[f:"q", fi_, _, mom_], F[f_, fi_, _, mom_]] := (* p^slash d_ij d_f1f2 *)
-  gammachain[slash[mom], spn[fi] // AmpConjugate, spn[fi]] delta[fun[fi], fun[fi] // AmpConjugate] deltaf[flv[fi], flv[fi] // AmpConjugate]
+  gammachain[slash[mom], spn[fi] // AmpConjugate, spn[fi]] *
+  delta[fun[fi], fun[fi] // AmpConjugate] *
+  deltaf[flv[fi], flv[fi] // AmpConjugate]
 CutAmplitudeGlue[F[f:"T", fi_, _, mom_], F[f_, fi_, _, mom_]] := (* (p^slash - mt1) d_ij d_f1f2 *)
-  (gammachain[slash[mom], spn[fi], spn[fi] // AmpConjugate] - mt1 gammachain[spn[fi], spn[fi] // AmpConjugate]) delta[fun[fi], fun[fi] // AmpConjugate] deltaft[flv[fi], flv[fi] // AmpConjugate]
+  (gammachain[slash[mom], spn[fi], spn[fi] // AmpConjugate] - mt1 gammachain[spn[fi], spn[fi] // AmpConjugate]) *
+  delta[fun[fi], fun[fi] // AmpConjugate] *
+  deltaft[flv[fi], flv[fi] // AmpConjugate]
 CutAmplitudeGlue[F[f:"t", fi_, _, mom_], F[f_, fi_, _, mom_]] := (* (p^slash + mt1) d_ij d_f1f2 *)
-  (gammachain[slash[mom], spn[fi] // AmpConjugate, spn[fi]] + mt1 gammachain[spn[fi] // AmpConjugate, spn[fi]]) delta[fun[fi], fun[fi] // AmpConjugate] deltaft[flv[fi], flv[fi] // AmpConjugate]
+  (gammachain[slash[mom], spn[fi] // AmpConjugate, spn[fi]] + mt1 gammachain[spn[fi] // AmpConjugate, spn[fi]]) *
+  delta[fun[fi], fun[fi] // AmpConjugate] *
+  deltaft[flv[fi], flv[fi] // AmpConjugate]
 
 (* Field styles for [[DiagramToGraphviz]].
  *)
